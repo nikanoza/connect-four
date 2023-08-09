60208574f57b90e2fd91e1948d6aed17f4c5ef01
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-09 19:21:36 +0400

    chore: cofigure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        night: "#000000",
+        "dark-violet": "#5C2DD5",
+        "semi-violet": "#7945FF",
+        pink: "#FD6687",
+        yellow: "#FFCE67",
+        light: "#ffffff",
+      },
+    },
   },
   plugins: [],
 };
