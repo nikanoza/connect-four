889f5664ca41a73042acd576f4fdfc3840ad490c
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-15 08:27:44 +0400

    feat: create red player turn component

--- src/components/TurnPanel.tsx
+++ src/components/TurnPanel.tsx
@@ -2,8 +2,14 @@ import { TurnRed } from "../svg";
 
 const TurnPanel = () => {
   return (
-    <div>
-      <TurnRed player={1} time={30} />
+    <div className="relative -mt-3">
+      <TurnRed />
+      <h3 className="text-light text-base font-bold absolute top-10 left-8">
+        PLAYER 1’S TURN
+      </h3>
+      <h2 className="text-light text-6xl font-bold absolute top-16 left-14">
+        15s
+      </h2>
     </div>
   );
 };
